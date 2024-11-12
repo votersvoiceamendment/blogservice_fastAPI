b6075753a574d738aa50eafdf8d57aab448f5c97
derekgygax <derekgygax@gmail.com>
2024-11-11 21:43:08 -0500

    routes generally added for post and comment

--- routers/comments.py
+++ routers/comments.py
@@ -1,9 +1,15 @@
 from typing import List
+from uuid import UUID
 from fastapi import APIRouter, HTTPException, status
-from ..models import Comment
+from models.comment import Comment
+from schemas.comment import CommentBase
 
 router = APIRouter(prefix="/api/v1/posts/{post_id}/comments")
 
 @router.get("/", response_model=List[Comment])
-async def get_comments():
+async def get_comments(post_id: UUID):
 	return ""
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def add_comment(post_id: UUID, comment: CommentBase):
+	return
--- routers/posts.py
+++ routers/posts.py
@@ -1,9 +1,27 @@
 from typing import List
+from uuid import UUID
 from fastapi import APIRouter, HTTPException, status
-from ..models import Post
+from schemas.post import PostBase, Post
 
 router = APIRouter(prefix="/api/v1/posts")
 
 @router.get("/", response_model=List[Post])
 async def get_posts():
-	return ""
+	return []
+
+@router.get("/ids", response_model=List[UUID])
+async def get_post_ids():
+	return []
+
+# @router.get("/{id}", response_model=Post)
+# async def get_post():
+# 	return 
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase):
+	print(post)
+	return
+
+@router.put("/{id}", status_code=status.HTTP_200_OK)
+async def update_post(id: UUID, updated_post: PostBase):
+	return
